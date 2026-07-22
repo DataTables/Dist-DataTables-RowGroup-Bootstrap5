584b0582c51040c29850104f85391affe16533f3
dtbuild <dtbuild@rigil>
2026-07-22 14:46:48 +0000

    f8eae909be69bd3f000913ac3f9672a8d765f734 Release 2.0.0 ready
    Sync to source repo @f8eae909be69bd3f000913ac3f9672a8d765f734

--- js/rowGroup.bootstrap5.min.mjs
+++ js/rowGroup.bootstrap5.min.mjs
@@ -1,4 +1,4 @@
-/*! RowGroup Bootstrap 5 styling 2.0.0-beta.1 for DataTables
+/*! RowGroup Bootstrap 5 styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs5";import RowGroup from"datatables.net-rowgroup";export default DataTable;
